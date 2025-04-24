3f9faf5b79026cd4ba600b4403490b5b790465e0
SteshaKrutova <157019026+SteshaKrutova@users.noreply.github.com>
2025-04-24 04:37:38 +0300

    Update models.py

--- device-analytics/api/models.py
+++ device-analytics/api/models.py
@@ -1,11 +1,17 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime
 from api.database import Base
 
+# Импортируем Base из database.py
+from api.database import Base
+
+# Определение модели данных для таблицы "data"
 class DeviceData(Base):
-    __tablename__ = "data"
-    id = Column(Integer, primary_key=True, index=True)
-    device_id = Column(String, index=True)
-    x = Column(Float)
-    y = Column(Float)
-    z = Column(Float)
-    timestamp = Column(DateTime)
+    __tablename__ = "data"  # Название таблицы в базе данных
+
+    # Определение колонок таблицы
+    id = Column(Integer, primary_key=True, index=True)  # Первичный ключ
+    device_id = Column(String, index=True)  # ID устройства
+    x = Column(Float)  # Координата X
+    y = Column(Float)  # Координата Y
+    z = Column(Float)  # Координата Z
+    timestamp = Column(DateTime)  # Временная метка
