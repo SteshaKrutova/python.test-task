e18508377e9e32ac1b31e8e2786b5dfdf9b6c8a9
SteshaKrutova <157019026+SteshaKrutova@users.noreply.github.com>
2025-04-24 04:29:20 +0300

    Update models.py

--- device-analytics/api/models.py
+++ device-analytics/api/models.py
@@ -1,25 +1,6 @@
-# Модели базы данных с использованием SQLAlchemy.
-from sqlalchemy import Column, Integer, String, Float, DateTime, create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from api.database import Base
 
-# Строка подключения
-DATABASE_URL = "postgresql+psycopg2://user:password@localhost:5432/mydb"
-
-# Создание движка
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Создание базового класса для моделей
-Base = declarative_base()
-
-# Тестирование подключения
-try:
-    with engine.connect() as connection:
-        print("Connection successful!")
-except Exception as e:
-    print(f"Error: {e}")
-
-# Модель данных устройства
 class DeviceData(Base):
     __tablename__ = "data"
     id = Column(Integer, primary_key=True, index=True)
